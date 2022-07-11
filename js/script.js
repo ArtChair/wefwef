499a7ead06647b78b5e632b028839a6e0196a2e6
Artem <104214210+ArtChair@users.noreply.github.com>
2022-07-11 19:55:09 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// var numOne = prompt ('Введите первое число:')
+// var numTwo = prompt ('Введите второе число:')
+// if ( numOne > numTwo) {
+//     document.write(numOne + ' больше ' + numTwo);
+// } else if (numOne < numTwo) {
+//     document.write(numOne + ' меньше ' + numTwo);
+// } else  {
+//     document.write(numOne + ' равно ' + numTwo)
+// }
+// let myName = {
+//     name: "Артем",
+//     age: 30,
+//     lifeStyle: 'school',
+//     addres: {
+//         city: 'Ust-Ilimsk',
+//         street: 'Mira29',
+//     }
+// }
+// console.log(myName)
